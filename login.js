e133f223456f4830bc46a5a69248af5353624fb1
CHINCHU THOMAS <85783531+chinchuthomas7@users.noreply.github.com>
2021-07-19 22:22:08 +0530

    Update login.js

--- login.js
+++ login.js
@@ -13,30 +13,37 @@ $(document).ready(function(){
         e.preventDefault();
         // method in jQuery to stop the default action of selected element to occur.
      
+        //         validating username and password
      
     if ($("#user").val()==""|| $("#password").val()=="")
     {
-        $("p").html("<h2>Enter the credentials</h2>")
+        $("p").html("<h5>Enter the credentials</h5>")
         $("p").css("size:5px")
 
     }
+//         when the username and password is empty,it must show a message like"enter the credentials.for this we are selecting the id names of user input and password input and
+//         we are using .val(),an inbuilt method in jQuery which set the value of them as empty.
+
+        
     else if ($("#user").val()=="admin" &&  $("#password").val()=="12345")
     {
         setTimeout(function()
         {
-            $("p").html("<h2> Success</h2>")
+            $("p").html("<h5> Success</h5>")
         },3000);
+ $("p").css("color:green")
        
 
         setTimeout(function()
         {
-            $(p).html("<h2> Verification in process</h2>");
+            $(p).html("<h5> Verification in process</h5>");
         },3000);
         
         access (todo)
     }
     else{
         $("p").html("<h2>wrong credentials</h2>")
+         $("p").css("color:red")
 
     }
     function access(callback)
